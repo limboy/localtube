6d2b612db4c4acdf4d02aa85f97238a4d1718405
Limboy <healdream@gmail.com>
2026-04-20 21:36:39 +0800

    refactor: update sidebar width CSS variables to modern syntax

--- src-web/src/components/ui/sidebar.tsx
+++ src-web/src/components/ui/sidebar.tsx
@@ -191,7 +191,7 @@ const Sidebar = React.forwardRef<
       return (
         <div
           className={cn(
-            "flex h-full w-[var(--sidebar-width)] flex-col bg-sidebar text-sidebar-foreground",
+            "flex h-full w-(--sidebar-width) flex-col bg-sidebar text-sidebar-foreground",
             className
           )}
           ref={ref}
@@ -213,25 +213,25 @@ const Sidebar = React.forwardRef<
       >
         <div
           className={cn(
-            "relative h-svh w-[var(--sidebar-width)] bg-transparent transition-[width] duration-150 ease-in-out",
+            "relative h-svh w-(--sidebar-width) bg-transparent transition-[width] duration-150 ease-in-out",
             "group-data-[collapsible=offcanvas]:w-0",
             "group-data-[side=right]:rotate-180",
             variant === "floating" || variant === "inset"
               ? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]"
-              : "group-data-[collapsible=icon]:w-[var(--sidebar-width-icon)]",
+              : "group-data-[collapsible=icon]:w-(--sidebar-width-icon)",
             isResizing && "transition-none"
           )}
         />
         <div
           className={cn(
-            "fixed inset-y-0 z-10 h-svh w-[var(--sidebar-width)] transition-[left,right,width] duration-150 ease-in-out flex",
+            "fixed inset-y-0 z-10 h-svh w-(--sidebar-width) transition-[left,right,width] duration-150 ease-in-out flex",
             side === "left"
               ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
               : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
             // Adjust the padding for floating and inset variants.
             variant === "floating" || variant === "inset"
               ? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]"
-              : "group-data-[collapsible=icon]:w-[var(--sidebar-width-icon)] group-data-[side=left]:border-r group-data-[side=right]:border-l border-sidebar-border!",
+              : "group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l border-sidebar-border!",
             isResizing && "transition-none",
             className
           )}
