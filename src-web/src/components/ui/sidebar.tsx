80a2e48a580a0574908d790cc83c4b671dbb2127
Limboy <healdream@gmail.com>
2026-04-21 10:20:29 +0800

    fix: increase sidebar z-index, remove redundant title, and disable drag region on resize handle

--- src-web/src/components/ui/sidebar.tsx
+++ src-web/src/components/ui/sidebar.tsx
@@ -233,7 +233,7 @@ const Sidebar = React.forwardRef<
         />
         <div
           className={cn(
-            "fixed inset-y-0 z-10 h-svh w-(--sidebar-width) transition-[left,right,width] duration-150 ease-in-out flex",
+            "fixed inset-y-0 z-20 h-svh w-(--sidebar-width) transition-[left,right,width] duration-150 ease-in-out flex",
             side === "left"
               ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
               : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
@@ -323,7 +323,6 @@ const SidebarRail = React.forwardRef<HTMLButtonElement, React.ComponentProps<"bu
         aria-label="Resize Sidebar"
         tabIndex={-1}
         onMouseDown={onMouseDown}
-        title="Resize Sidebar"
         className={cn(
           "absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 transition-all ease-linear after:absolute after:inset-y-0 after:left-1/2 after:w-0.5 hover:after:bg-sidebar-border group-data-[side=left]:-right-4 group-data-[side=right]:left-0 sm:flex",
           "in-data-[side=left]:cursor-ew-resize in-data-[side=right]:cursor-ew-resize",
@@ -331,6 +330,7 @@ const SidebarRail = React.forwardRef<HTMLButtonElement, React.ComponentProps<"bu
           "group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full group-data-[collapsible=offcanvas]:hover:bg-sidebar",
           "[[data-side=left][data-collapsible=offcanvas]_&]:-right-2",
           "[[data-side=right][data-collapsible=offcanvas]_&]:-left-2",
+          "[app-region:no-drag] [-webkit-app-region:no-drag]",
           className
         )}
         {...props}
